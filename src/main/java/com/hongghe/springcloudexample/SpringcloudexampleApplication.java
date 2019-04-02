63ec2dd5661455469d5407e35073bc82ba5d7080
henry <zhanghongheman@gmail.com>
2019-04-02 23:26:24 +0800

    Initial commit

---
+++ src/main/java/com/hongghe/springcloudexample/SpringcloudexampleApplication.java
@@ -0,0 +1,13 @@
+package com.hongghe.springcloudexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudexampleApplication.class, args);
+    }
+
+}
