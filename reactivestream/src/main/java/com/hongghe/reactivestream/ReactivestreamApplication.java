4f95af31a70c52e3b5774aae5632926d6406dfa7
hongghe <364930855@qq.com>
2019-04-04 18:53:56 +0800

    修改项目结果。

---
+++ reactivestream/src/main/java/com/hongghe/reactivestream/ReactivestreamApplication.java
@@ -0,0 +1,13 @@
+package com.hongghe.reactivestream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactivestreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactivestreamApplication.class, args);
+    }
+
+}
